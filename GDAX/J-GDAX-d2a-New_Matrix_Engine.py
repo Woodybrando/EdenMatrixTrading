ff1aaca77b932003eaed146242ecedfbc0711006
woodybrando <jayson.rahmlow@gmail.com>
2017-11-24 17:35:00 +0100

    It's coming Alive! Jayson - 11/24/17 - Matrix Upper Market Buy working, Matrix upper sell order placement working... tbc

--- GDAX/J-GDAX-d2a-New_Matrix_Engine.py
+++ GDAX/J-GDAX-d2a-New_Matrix_Engine.py
@@ -58,16 +58,11 @@ loopit = True
 
 #run_once = 0
 
+LTC_USD = 'LTC-USD'
 
+LTC_BTC = 'LTC-BTC'
 
-
-
-
-LTC_USD = 'LTC_USD'
-
-LTC_BTC = 'LTC_BTC'
-
-BTC_USD = 'BTC_USD'
+BTC_USD = 'BTC-USD'
 
 
 a = LTC_USD
@@ -79,78 +74,340 @@ B = LTC_BTC
 c = BTC_USD
 C = BTC_USD
 
-marketP = input("Enter the letter of the market pair u want to trade in? a. LTC_USD b. LTC_BTC c. BTC_USD?")
+x = 0
 
-if marketP == a:
+y = 1
+
+marketPair = input("Enter the letter of the market pair u want to trade in? a. LTC_USD b. LTC_BTC c. BTC_USD?")
+
+if marketPair == a:
     marketDec = 2
-elif marketP == b:
+elif marketPair == b:
     marketDec = 6
-elif marketP == c:
+elif marketPair == c:
     marketDec = 2
 
-marketP = input("What is the current market price?")
 
-GET /products/<product-id>/ticker
+# Add a way to set what entry point u want to start your matrix say if market is 75.5
+# and u want to start only if price = 73
 
 
-print("Current Market Price is: " + str(marketP))
 
-marketTop = marketP * 1.08
+marketPr = {}
+
+howPrice = input("Do u want to x. enter the market price or y. ask GDAX? x or y?")
+
+if howPrice == x:
+    marketP = input("What is the current market price?")
+
+
+
+elif howPrice == y:
+    marketPr = requests.get(api_url + '/products/LTC-USD/ticker')
+    jsonPrice = marketPr.json()
+    print(jsonPrice['ask'])
+    marketPr = float(jsonPrice['ask'])
+    marketP = round(marketPr, 2)
+
+#requests.post(api_url + '/orders', json=order, auth=auth)
+
+initInv = input("How much do you want to invest, and by invest I mean how much can u stand to lose???")
+
+
+aboveInv = initInv/2
+
+print("Above Investment: " + str(aboveInv))
+
+
+
+aboveFee = aboveInv * .003
+
+upperBuy = aboveInv - aboveFee
+
+aboveInv = upperBuy
+
+print("Upper Cost minus Fee " + str(upperBuy))
+
+
+aboveCoins = aboveInv/marketP
+
+print( "Above Coins: " + str(aboveCoins))
+
+# turn this on as a question in the future
+resolution = 25
+
+aboveVol = round(aboveCoins/(resolution - 1), 4)
+
+print("Above Coin Volume = " + str(aboveVol))
+
+belowBuys = initInv - aboveInv
+
+print("Below Investment: " + str(belowBuys))
+
+unroundVal = belowBuys/(resolution - 1)
+
+belowVal = round(unroundVal, 4)
+
+print("Below Value Per Peg: " + str(belowVal))
+
+
+marketTop = marketP * 1.12
 
 print("Market Top " + str(marketTop))
 
-count = marketTop
+
 
 marketBottom = .92 * marketP
 
 print("Market Bottom " +  str(marketBottom))
 
+marketUpper = marketP * 1.002
+marketLower = marketP * .998
+
+print("MarketUpper is " + str(marketUpper))
+print("MarketLower is " + str(marketLower))
+
+
+upperSpread = (marketTop - marketUpper) / (resolution - 1)
+lowerSpread = (marketLower - marketBottom) / (resolution - 1)
+
+print("Lower Spread " + str(lowerSpread))
+
 peg = 0
-number = 1
+number = 0
 list = []
 matrix = []
+upperMatrix = []
+lowerMatrix = []
+countOrder = {}
 
-while count > marketBottom:
-    peg = count * .997
-    volume = .13 + (.3 - (count * .003))
-    rPeg = round(peg, marketDec)
-    strPeg = str(rPeg)
-    pegDollar, pCode = strPeg.split('.')
-    iDollar = int(pegDollar)
-    iCode = int(pCode)
-    if iCode == 98:
-        rPeggle = .97
-        rPeg = iDollar + rPeggle
+count = resolution * 2 - 1
 
-    if iCode == 45:
-        rPeggle = .44
-        rPeg = iDollar + rPeggle
+pegMakerU = marketP
+pegMakerL = marketP
+
+lNumber = 24
+mNumber = 25
+uNumber = 26
+
+print("Count is " + str(count))
+
+print("Current Market Price: " + str(marketPr))
+
+while count > -1:
+
+    peg = pegMakerU + upperSpread
+
+    #print("Count is " + str(count))
+    #print("Peg is " + str(peg))
+
+    if count > resolution:
+
+        volume = aboveVol
+
+        rPeg = round(peg, marketDec)
+
+        strPeg = str(rPeg)
+
+        pegDollar, pCode = strPeg.split('.')
+
+        iDollar = int(pegDollar)
+
+        iCode = int(pCode)
+
+        if iCode == 98:
+            rPeggle = .97
+            rPeg = iDollar + rPeggle
+
+        if iCode == 45:
+            rPeggle = .44
+            rPeg = iDollar + rPeggle
+
+        list = uNumber, rPeg, round(volume, 4)
+        matrix.append(list)
+        upperMatrix.append(list)
+        pegMakerU = peg
+        uNumber = uNumber + 1
+
+    elif count == 25:
+        peg = marketP
+        rPeg = round(peg, 2)
+        volume = aboveVol
+        number = mNumber
+        list = number, rPeg, round(volume, 4)
+        matrix.append(list)
+
+    elif count < resolution:
+        peg = pegMakerL - lowerSpread
+
+        if peg >= (marketBottom):
+
+            belowVol = belowVal / peg
+            volume = belowVol
+
+            rPeg = round(peg, marketDec)
+
+            strPeg = str(rPeg)
+
+            pegDollar, pCode = strPeg.split('.')
+
+            iDollar = int(pegDollar)
+
+            iCode = int(pCode)
+
+            if iCode == 98:
+                rPeggle = .97
+                rPeg = iDollar + rPeggle
+
+            if iCode == 45:
+                rPeggle = .44
+                rPeg = iDollar + rPeggle
+
+            list = lNumber, rPeg, round(volume, 4)
+            matrix.append(list)
+            lowerMatrix.append(list)
+            pegMakerL = pegMakerL - lowerSpread
+            lNumber = lNumber - 1
 
 
-    rVol = round(volume, 3)
-    list = number, rPeg, rVol
-    matrix.append(list)
 
-    #print(number, rPeg, rVol)
+    # print(number, rPeg, rVol)
 
-    count = count - (count - peg)
-    number = number + 1
+    count = count - 1
 
-rowVal = []
+
+
+matrix.sort(key=lambda x: x[1])
+
+
+print(matrix)
+print(len(matrix))
+
+print(upperMatrix)
+print(len(upperMatrix))
+
+print(lowerMatrix)
+print(len(lowerMatrix))
+
+
+'''
+# To get the peg value printed uncomment this section
 
 for line in matrix:
-    lineVal = line[1] * line[2]
-    print(line)
-    print(lineVal)
-    rowVal.append(lineVal)
+
+    pegValue2 = line[1] * line[2]
+    print(round(pegValue2, 3))
+    
+print("Sum of row Values " + str(sum(rowVal)))
+'''
 
 
 totalInvestment = [ sum(x) for x in zip(*matrix) ]
+totalUpperInvestment = [ sum(x) for x in zip(*upperMatrix) ]
+totalLowerInvestment = [ sum(x) for x in zip(*lowerMatrix) ]
+
+print("Total Upper Investment in Coins? " + str(totalUpperInvestment[2]))
 #totalInvestment = sum(matrix)
 
-print("Total Investment @ 1 LTC Vol: " + str(totalInvestment))
+upperCost = totalUpperInvestment[2] * marketP
 
-print("Sum of row Values " + str(sum(rowVal)))
+print("Upper coin cost " + str(upperCost) )
+
+
+sumLower = []
+
+for line in lowerMatrix:
+    pegVal = line[1] * line[2]
+    rPegVal = round(pegVal, 3)
+    sumLower.append(rPegVal)
+
+lowerValAdd = sum(sumLower)
+
+print(sumLower)
+
+print("Lower buys total cost " + str(lowerValAdd))
+
+totalMatrixCost = upperCost + lowerValAdd
+
+print("Total Matrix Cost " + str(totalMatrixCost))
+
+
+
+#print("Total Coin Cost: " + str(upperCost + lowerCost))
+
+y = 0
+n = 1
+upperPegOrder = {}
+
+exitAns = input("Do you want to exit? y or n?")
+
+if exitAns == y:
+    exit()
+
+#for line in matrix:
+
+buyAbove = input("Do you want to buy your above matrix now? y or n?")
+
+if buyAbove == y:
+
+    marketOrder = str(round(aboveInv, 2))
+    print(marketOrder)
+
+    rAboveCoins = str(round(aboveCoins, 3))
+
+    buyNowAns = input(
+        "Does that number look right, aka you will buy " + rAboveCoins + " " + str(marketPair)
+        + " @ the current market price " + str(marketP)
+        + " if you say yes? y or n?")
+
+    if buyNowAns == y:
+        aboveOrder = {'type': 'market', 'funds': marketOrder, 'product_id': str(marketPair), 'side': 'buy'}
+
+        s = requests.post(api_url + '/orders', json=aboveOrder, auth=auth)
+
+        print(s.json())
+
+
+
+buildUpper = input("Do you want to build your upper matrix now? y or no?")
+
+if buildUpper == n:
+    exit()
+
+
+
+elif buildUpper == y:
+
+    for line in upperMatrix:
+        upperSide = 'sell'
+        upperVol = str(aboveVol)
+        upperPeg = str(line[1])
+        upperPegOrder = {'side': upperSide, 'price': upperPeg, 'size': upperVol, 'product_id': str(marketPair)}
+        t = requests.post(api_url + '/orders', json=upperPegOrder, auth=auth)
+        print(t.json())
+
+
+exit()
+
+for line in matrix:
+    while line[1] > marketBottom:
+        if line[1] >= marketUpper:
+            side = 'sell'
+        elif abs(line[1] - marketP) < abs(marketUpper - marketLower):
+            side = 0
+        elif line[1] <= marketLower:
+            side = 'buy'
+
+    if side != 0:
+        matrixOrder = { 'price': str(line[1]), 'size': str(line[2]), 'side': str(side), 'product_id': str(marketPair)}
+
+        print(order)
+
+        r = requests.post(api_url + '/orders', json=matrixOrder, auth=auth)
+
+
+
+
 
 y = 0
 n = 1
@@ -159,7 +416,7 @@ exitAns = input("Do you want to exit? y or n?")
 
 if exitAns == y:
     exit()
-else:
+elif exitAns == n:
     while loopit == True:
         run_once = 0
         last_order_file_handle = open('GDAX/last_order_id_processed.txt', 'r+')
